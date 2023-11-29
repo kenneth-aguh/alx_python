b3228b44701f78d9945147112f9a89dd20f76b9d
kenneth aguh <kennethpro2012@gmail.com>
2023-11-29 23:02:56 +0100

    kk

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -5,4 +5,4 @@ word_last_2 = word[7:]
 middle_word = word[1:8]
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
-print("Middle words: {}".format(middle_word), end="\n")
+print("Middle word: {}".format(middle_word), end="\n")
