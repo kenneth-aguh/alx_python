676e584b9ca5d51f6c01c2d7d07160d0b9827d55
kenneth aguh <kennethpro2012@gmail.com>
2023-12-07 22:09:22 +0100

    check

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 from add_0 import add
 
-if __name__ == "__main__":
 
 a = 1
 b = 2
-
-print("{} + {} = {}".format(a, b, add(a,b)))
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add(a,b)))
