abb18dfed17e5760bda9b30438719537afecd5ed
kenneth aguh <kennethpro2012@gmail.com>
2023-12-13 21:18:05 +0100

    getter and setter

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,23 +1,25 @@
-#!/usr/bin/env python3class Square:
+#!/usr/bin/env python3
 
 # this is the class Square
-
- 
 class Square:
     # specifying the attributes using __init__ 
     def __init__(self, size):
         # initializing a square instance
-
         # parameter - size
         # no type / value verification was used
-        
         self.__size = size
-        
     
     # getters
-   
     def get_size(self):
         return self.__size
     
-    # def set_size(self, value):
-    #         self.__size = value
+    # setter
+    def set_size(self, value):
+        self.__size = value
+
+# Example usage:
+square_instance = Square(5)
+print(square_instance.get_size())  # Output: 5
+
+square_instance.set_size(7)
+print(square_instance.get_size())  # Output: 7
