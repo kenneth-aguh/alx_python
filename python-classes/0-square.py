4ddfcc05075cd7c08546eacab3bfd773dbf7e255
kenneth aguh <kennethpro2012@gmail.com>
2023-12-13 18:00:25 +0100

    corection

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,15 @@
-class Square:
-    def __init__(self, size):
-        self.__size = size
+# the class Square
 
+ 
+class Square:
+    # specifying the attribute __init__ 
+    def __init__(self, size):
+        # initialize a square instance. parameter - size with no type / value verification was used
+        
+        self.__size = size
+        
+    
+    # define the getter
+   
     def get_size(self):
         return self.__size
