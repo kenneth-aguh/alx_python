815545bf1c4301e7dd97ed4aa099d0d4a3eeeb92
kenneth aguh <kennethpro2012@gmail.com>
2023-12-14 14:15:37 +0100

    corrected

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,25 +1,18 @@
 #!/usr/bin/env python3
-# make the class Square.
+''' make the class Square.'''
 
 class Square:
-    # state the attributes using __init__ 
+    '''state the attributes using __init__ '''
     def __init__(self, size):
-        # initialize a square instance
-        # parameter - size
-        # no type / value verification was used
+        '''initialize a square instance'''
+        '''parameter - size'''
+        '''no type / value verification was used'''
         self.__size = size
     
-    # getters
+    '''getters'''
     def get_size(self):
         return self.__size
     
-    # setter
+    '''setter'''
     def set_size(self, value):
         self.__size = value
-
-# Example:
-#my_square = Square(5)
-#print(my_square.get_size()) 
-
-#my_square.set_size(7)
-#print(my_square.get_size())  
