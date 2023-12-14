71a7670d2c9b10a640b55a34d801810c1bacc2ea
kenneth aguh <kennethpro2012@gmail.com>
2023-12-14 14:18:12 +0100

    corrected

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,7 +2,7 @@
 ''' make the class Square.'''
 
 class Square:
-    '''state the attributes using __init__ '''
+    '''state the attributes using __init__'''
     def __init__(self, size):
         '''initialize a square instance'''
         '''parameter - size'''
