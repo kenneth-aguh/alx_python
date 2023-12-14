0317bc45c4d958864e873cf94f2d099e7259a24b
kenneth aguh <kennethpro2012@gmail.com>
2023-12-14 21:10:56 +0100

    corrected

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
+''' make the class Square.'''
 
 class Square:
-    def __init__(self, size=0):
-        # Check if is an integer
+    '''state the attributes using __init__'''
+    def __init__(self, size):
+        '''initialize a square instance'''
+        '''parameter - size'''
+        '''no type / value verification was used'''
+        '''Set the size attribute'''
+        self.__size = size
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         
-        # Check if is less than 0
+        '''Check if is less than 0'''
         if size < 0:
-            raise ValueError("size must be >= 0")
-        
-        # Set the size attribute
-        self.__size = size
+            raise ValueError("size must be >= 0")  
