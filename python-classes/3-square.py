15e192ed4e48423a1a798b2cb8c2406c88de77ca
kenneth aguh <kennethpro2012@gmail.com>
2023-12-14 21:44:27 +0100

    corected

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -23,11 +23,11 @@ class Square:
         return self.__size**2
 
     '''getters'''
-    def size(self):
+    def get_size(self):
         return self.__size
     
     '''setter'''
-    def size(self, value):
+    def set_size(self, value):
         '''Set the size attribute'''
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
