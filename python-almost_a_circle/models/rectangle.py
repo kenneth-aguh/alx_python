28df3ed10c508e478c22fc89727277a2b643ef01
kenneth aguh <kennethpro2012@gmail.com>
2024-01-03 15:36:36 +0100

    corrected

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,7 +8,7 @@ class Rectangle(Base):
     ''' The class constructor (__init__) calls the super class 
     with the provided id and assigns the other arguments 
     (width, height, x, y) to the appropriate attributes'''
-    
+
     def __init__(self, width, height, x=0, y=0, id=None):
         '''call the super class with id'''
         super().__init__(id)
@@ -18,42 +18,58 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
-    '''width getter'''
+    '''width getter - Using getters and setters provides a 
+    way to validate and control the assignment of values to the attributes, 
+    adding a layer of protection'''
     @property
     def width(self):
         return self.__width
 
-    '''width setter'''
+    '''width setter -Using getters and setters provides a 
+    way to validate and control the assignment of values to the attributes, 
+    adding a layer of protection'''
     @width.setter
     def width(self, value):
         self.__width = value
 
-    '''height getter'''
+    '''height getter - Using getters and setters provides a 
+    way to validate and control the assignment of values to the attributes, 
+    adding a layer of protection'''
     @property
     def height(self):
         return self.__height
 
-    '''height setter'''
+    '''height setter - Using getters and setters provides a 
+    way to validate and control the assignment of values to the attributes, 
+    adding a layer of protection'''
     @height.setter
     def height(self, value):
         self.__height = value
 
-    '''x getter'''
+    '''x getter- Using getters and setters provides a 
+    way to validate and control the assignment of values to the attributes, 
+    adding a layer of protection'''
     @property
     def x(self):
         return self.__x
 
-    '''x setter'''
+    '''x setter - Using getters and setters provides a 
+    way to validate and control the assignment of values to the attributes, 
+    adding a layer of protection'''
     @x.setter
     def x(self, value):
         self.__x = value
 
-    ''' y getter'''
+    ''' y getter - Using getters and setters provides a 
+    way to validate and control the assignment of values to the attributes, 
+    adding a layer of protection'''
     @property
     def y(self):
         return self.__y
 
-    '''y setter '''
+    '''y setter -Using getters and setters provides a 
+    way to validate and control the assignment of values to the attributes, 
+    adding a layer of protection'''
     @y.setter
     def y(self, value):
         self.__y = value
