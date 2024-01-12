4473c468470bf5d8eea20cf199e118be6956e6bd
kenneth aguh <kennethpro2012@gmail.com>
2024-01-12 18:54:42 +0100

    changes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,42 +1,36 @@
 '''Import the base module'''
 from models.base import Base
 
-'''
-The Rectangle class inherits from the Base class.
-'''
+'''The Rectangle class inherits from the Base class.'''
 class Rectangle(Base):
     ''' The class constructor (__init__) calls the super class 
     with the provided id and assigns the other arguments 
     (width, height, x, y) to the appropriate attributes'''
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        '''call the super class with id'''
+        
         super().__init__(id)
-        '''assign each argument to the right attribute'''
-        self.width = width
-        '''assign each argument to the right attribute'''
-        self.height = height
-        '''assign each argument to the right attribute'''
-        self.x = x
-        '''assign each argument to the right attribute'''
-        self.y = y
-    '''creating separate methods for integer validation'''
+    
+        self.__width = width
+
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
     def __validate_integers(self, value, attribute_name):
-        '''lets state the condition for validation'''
         if not isinstance(value, int):
             raise TypeError(f"{attribute_name} must be an integer")
-    '''creating separate methods for positives validation'''
+        
     def __validate_positives(self, value, attribute_name):
-        '''lets state the condition for validation'''
         if value <= 0:
             raise ValueError(f"{attribute_name} must be > 0")
 
-    '''width getter - Using getters and setters provides a way to validate and 
-    control the assignment of values to the attributes, 
-    adding a layer of protection'''
+    '''adding a layer of protection'''
     @property
     def width(self):
         return self.__width
+    
+
 
     '''width setter -Using getters and setters provides a 
     way to validate and control the assignment of values to the attributes, 
